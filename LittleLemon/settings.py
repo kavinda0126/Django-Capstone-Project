0cfbd72d13c0883c32080888bd8e9a477ba5c3d5
kavindad01 <139451842+kavindad01@users.noreply.github.com>
2023-12-20 02:50:19 +0530

    Update settings.py

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',   
         'NAME': 'littlelemon',   
         'USER': 'root',   
-        'PASSWORD': 'sliit123',   
+        'PASSWORD': 'root',   
         'HOST': '127.0.0.1',   
         'PORT': '3306',   
         'OPTIONS': {   
